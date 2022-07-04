51f4927b029374edad8207c8395c93feb80d22e3
Anand.Narayan <arnarayan@gmail.com>
2022-07-04 16:08:49 -0600

    Initial Commit

---
+++ YoutubeApp/YoutubeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  YoutubeAppApp.swift
+//  YoutubeApp
+//
+//  Created by Anand Narayan on 2022-07-04.
+//
+
+import SwiftUI
+
+@main
+struct YoutubeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
